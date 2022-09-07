329158064a006c41cbae48930dc8aa16be7cad4a
Tomás <tomasezcardozo@gmail.com>
2022-09-07 20:00:33 -0300

    Update mainCode.py

--- mainCode.py
+++ mainCode.py
@@ -1,21 +1,36 @@
 import keyboard
 import datetime
 
-class Keylogger:
-    def __init__(self, definedIntervalTime = int):
-        """
-        This function set a empty string and the actual date
-        for every individual string in a certain period of time
 
-        """
-        self.definedIntervalTime = definedIntervalTime
-        self.logText = ""
-        self.date = datetime.now()
+def __init__(self, maxTime = 5, definedIntervalTime = int):
+    """
+    This function set a empty string and the actual date
+    for every individual string in a certain period of time
+
+    """
+    self.definedIntervalTime = definedIntervalTime
+    self.logText = ""
+    self.date = datetime.now()
 
 
-    def record_log():
-        """
-        This function will read each keypressed and register it
-        on a string
-        """
-        pass
+def record_log():
+    """
+    This function will read each keypressed and register it
+    on a string
+    """
+    recorded = ""
+    recorded = keyboard.record(until='enter')
+    print(recorded)
+
+record_log()
+
+# def main():
+#     list_of_strings = []
+#     while len(list_of_strings) < 5:
+#         finalString = record_log()
+#         finalString.append(list_of_strings)
+#     print(list_of_strings)
+
+# if __name__ == "__main__":
+#     print("ejecucion")
+#     main()
